7ccb8feb0fa63dcb17d1006ee70519090a4ae5db
WooYeonSeo <vantovan7414@gmail.com>
2026-05-06 16:51:57 +0900

    Initial Commit

---
+++ puppy/puppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  puppyApp.swift
+//  puppy
+//
+//  Created by Yeon Seo Woo on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct puppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
